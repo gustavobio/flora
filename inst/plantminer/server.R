99a08009f567581d78e944576ce84eb9743582e9
Gustavo Carvalho <gustavo.bio@gmail.com>
2014-06-28 10:19:22 -0300

    Including a shiny app

---
+++ inst/plantminer/server.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(flora)
+ex <- get.taxa(c("Miconia albicans", "Myrcia lingua", "Cofea arabica"))
+shinyServer(function(input, output) {
+  output$contents <- renderDataTable({
+    if (input$taxa == "Miconia albicans\nMyrcia lingua\nCofea arabica" & input$synonyms & input$suggest) {
+      res <- ex
+    } else {
+      x <- unlist(strsplit(input$taxa, "\n"))
+      res <- get.taxa(x, 
+                      replace.synonyms = input$synonyms, 
+                      suggest.names = input$suggest, 
+                      life.form = input$life.form, 
+                      habitat = input$habitat, 
+                      vernacular = input$vernacular, 
+                      states = input$states, 
+                      establishment = input$establishment)
+    }
+    output$downloadData <- downloadHandler(
+      filename = "results.csv",
+      content = function(file = filename) {      
+        # Write to a file specified by the 'file' argument
+        write.csv(res, file,
+                  row.names = FALSE, quote = TRUE)
+      }
+    )
+    
+    links <- 
+      paste("<a target=\"_blank\" href = \"http://floradobrasil.jbrj.gov.br/jabot/listaBrasil/FichaPublicaTaxonUC/FichaPublicaTaxonUC.do?id=FB", res$id, "\">", res$id,"</a>", sep = "")
+    links <- gsub("FBNA", NA, links)
+    out <- data.frame(id = links, res[-1])
+    names(out) <- gsub("\\.", " ", names(out))
+    out    
+  }, options = list(bFilter = TRUE))
+})
---
+++ inst/plantminer/ui.R
@@ -0,0 +1,31 @@
+shinyUI(fluidPage(  
+  h1("Plantminer"),
+  sidebarLayout(
+    sidebarPanel(width = 3,
+                 checkboxInput("synonyms", label = "Replace synonyms", value = TRUE),
+                 checkboxInput("suggest", label = "Correct misspelled names", value = TRUE),
+                 checkboxInput("life.form", label = "Life form", value = FALSE),
+                 checkboxInput("habitat", label = "Habitat", value = FALSE),
+                 checkboxInput("vernacular", label = "Vernacular names", value = FALSE),
+                 checkboxInput("states", label = "Occurrence", value = FALSE),
+                 checkboxInput("establishment", label = "Establishment", value = FALSE),
+                 tags$form(
+                   tags$textarea(id="taxa", rows=16, cols=5, "Miconia albicans\nMyrcia lingua\nCofea arabica"),
+                   tags$br(),
+                   tags$input(type = "Submit"),
+                   tags$i("(This may take a while)"))
+    ),
+    mainPanel(width = 9,
+              h5("Data"),
+              p("This application is an alternative front end for the",
+              tags$a(href = "http://cran.r-project.org/package=flora", "flora"),
+              "package for R. All data used here was kindly made available by the ",
+              tags$a(href = "http://floradobrasil.jbrj.gov.br", "Brazilian Flora Checklist"),
+              " project. Please cite them accordingly. Send your suggestions and report bugs to Gustavo Carvalho at gustavo.bio@gmail.com."
+              ),
+              h5("Usage"),
+              p("Usage is simple: paste your taxa without authors in the textbox and hit submit. Click on the id to open the species page on the Brazilian Flora Checklist. There is a download button below to export data as a quoted csv file."),
+              dataTableOutput(outputId="contents"),
+              downloadButton('downloadData', 'Download results in csv format')
+    ))
+))
