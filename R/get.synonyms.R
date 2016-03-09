22d728416fae4acd0ca8c668b7277dd9ca75f025
Gustavo Carvalho <gustavo.bio@gmail.com>
2016-03-09 15:27:44 -0300

    New function to get all synonyms of a given taxon

---
+++ R/get.synonyms.R
@@ -0,0 +1,32 @@
+#' List all synonyms of a given taxa
+#' 
+#' This function returns all the synonyms of a given taxon according to the Brazilian Flora 2020.
+#' 
+#' 
+#' @param taxon a character vector containing a name.
+#' 
+#' @return a character vector
+#' @export
+#' @examples 
+#' \dontrun{
+#' synonyms("Myrcia lingua")
+#' }
+
+get.synonyms <- function(taxon) {
+  if (length(taxon) == 0) stop("Please provide a name.")
+  if (length(taxon) > 1) stop("Please provide only one name")
+  processed_taxon <- get.taxa(taxon, replace.synonyms = FALSE, suggest.names = FALSE)
+  if (is.na(processed_taxon$id)) {
+    return(NA)
+  }
+  else {
+    synonyms <- relationships[with(relationships, {
+      related.id == processed_taxon$id & grepl("É sinônimo", relationship)
+      }), ]
+  }
+  if (NROW(synonyms) == 0) {
+    return(NULL)
+  } else {
+    return(all.taxa[all.taxa$id %in% synonyms$id, ]$search.str)
+  }
+}
