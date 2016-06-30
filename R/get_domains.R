a4f815b0275e78d6e7f343e52c92b23116a49c6d
Gustavo Carvalho <gustavo.bio@gmail.com>
2016-06-30 17:52:08 -0300

    Include a function to get phytogeographic domains

---
+++ R/get_domains.R
@@ -0,0 +1,26 @@
+#' Get the phytogeographic domain of a list of taxa
+#'
+#' @param taxa 
+#'
+#' @return A data frame as returned by get.taxa with an extra column named domain.
+#' @export
+#'
+#' @examples
+#' \dontrun {
+#' taxa <- get.taxa(c("Myrcia guianensis", "bleh", "Xylopia", "Miconia albicans", "bleh", "Poa annua")
+#' get_domains(taxa)
+#' }
+get_domains <- function(taxa) {
+  if (!requireNamespace("jsonlite", quietly = TRUE)) {
+    stop("Package jsonlite needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  get_sp_domain <- function(id) {
+    if (is.na(id)) return(NA)
+    sp_info <- fromJSON(paste0("http://floradobrasil.jbrj.gov.br/reflora/listaBrasil/ConsultaPublicaUC/ResultadoDaConsultaCarregaTaxonGrupo.do?&idDadosListaBrasil=", id))
+    paste(sp_info$dominioFitogeografico, collapse = "|")
+  }
+  domains <- sapply(taxa$id, get_sp_domain)
+  taxa$domain <- domains
+  taxa
+}
