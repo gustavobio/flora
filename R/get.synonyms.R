bdb0f1045dbab96c20d4b2e954bb9f63a6ad5f09
Gustavo <gustavo.bio@gmail.com>
2020-04-15 23:36:56 -0300

    Bug fixes and new data

--- R/get.synonyms.R
+++ R/get.synonyms.R
@@ -5,21 +5,21 @@
 #' 
 #' @param taxon a character vector containing a name.
 #' @param fix should the function fix issues (synonyms, mispelled names) in taxon?
+#' @param relationship return the kind of relationship?
 #' @return a character vector
 #' @export
 #' @examples
 #' \dontrun{
-#' synonyms("Myrcia lingua")
+#' get.synonyms("Myrcia lingua")
 #' }
 
-get.synonyms <- function(taxon, fix = FALSE) {
+get.synonyms <- function(taxon, fix = FALSE, relationship = FALSE) {
   if (length(taxon) == 0) stop("Please provide a name.")
   if (length(taxon) > 1) stop("Please provide only one name")
   if (fix) {
     processed_taxon <- get.taxa(taxon, replace.synonyms = TRUE, suggest.names = TRUE)
   } else {
     processed_taxon <- get.taxa(taxon, replace.synonyms = FALSE, suggest.names = FALSE)
-    
   }
   if (is.na(processed_taxon$id)) {
     return(NA)
@@ -33,6 +33,14 @@ get.synonyms <- function(taxon, fix = FALSE) {
   if (NROW(synonyms) == 0) {
     return(NULL)
   } else {
-    return(all.taxa[all.taxa$id %in% synonyms$id, ]$search.str)
+    res <- all.taxa[all.taxa$id %in% synonyms$id, ]
+    if (relationship) {
+      rl <- relationships[relationships$id %in% processed_taxon$id, ]
+      rl_spp <- all.taxa[all.taxa$id %in% rl$related.id, ]
+      rl_all <- merge(rl, rl_spp, by.x = "related.id", by.y = "id")
+      return(data.frame(spp = processed_taxon$search.str, synonym = rl_all$search.str, relationship = rl_all$relationship))
+    } else {
+      return(res$search.str)
+    }
   }
 }
